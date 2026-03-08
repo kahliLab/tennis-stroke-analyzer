bec3d3c05ad8d3198a59dc77afe91f00a7cee637
k2 <klaus.kahlfuss@kstp.at>
2026-03-08 21:28:27 +0100

    feat: add get_landmarks

--- src/tennis_analyzer/pose_tracker.py
+++ src/tennis_analyzer/pose_tracker.py
@@ -1,4 +1,5 @@
 import mediapipe as mp
+import cv2
 
 
 def init_pose():
@@ -6,3 +7,14 @@ def init_pose():
     pose = mp_pose.Pose()
 
     return pose
+
+
+def get_landmarks(frame, pose):
+    fixed_frame = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
+
+    result = pose.process(fixed_frame)
+
+    if not result.pose_landmarks:
+        return None
+
+    return result
