2d6d868635dea5b1786c4f05a21f7b7d43c126c5
k2 <7385304-kahliLab@users.noreply.gitlab.com>
2026-03-08 18:58:25 +0100

    feat: add extract_frames

--- src/tennis_analyzer/video_processor.py
+++ src/tennis_analyzer/video_processor.py
@@ -8,3 +8,16 @@ def load_video(path):
         raise ValueError(f"Video {path} konnte nicht geöffnet werden.")
 
     return cap
+
+def extract_frames(cap):
+    frames = []
+
+    while True:
+        ret, frame = cap.read()
+        if not ret:
+            break
+        frames.append(frame)
+
+    cap.release()
+
+    return frames
