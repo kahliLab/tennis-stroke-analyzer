479d310cfc0ae1835f941c3ad832837b925732ca
k2 <klaus.kahlfuss@kstp.at>
2026-03-13 18:49:34 +0100

    feat: add put_text for annotate text to frame

--- src/tennis_analyzer/overlay.py
+++ src/tennis_analyzer/overlay.py
@@ -5,3 +5,16 @@ STROKE_COLOUR = {
     "flat": (0, 255, 0),
     "slice": (255, 0, 0)
 }
+
+def put_text(stroke, frame):
+    frame_with_text = cv2.putText(
+        frame,
+        stroke,
+        (10, 30),
+        cv2.FONT_HERSHEY_SIMPLEX,
+        1,
+        STROKE_COLOUR[stroke],
+        2
+    )
+
+    return frame_with_text
