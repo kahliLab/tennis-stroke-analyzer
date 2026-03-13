c19ae118c8bda6fbbeb912a0ad9b6d974b9e5ee7
k2 <klaus.kahlfuss@kstp.at>
2026-03-13 19:20:29 +0100

    feat: add function to draw line to frame

--- src/tennis_analyzer/overlay.py
+++ src/tennis_analyzer/overlay.py
@@ -18,3 +18,24 @@ def put_text(stroke, frame):
     )
 
     return frame_with_text
+
+
+def draw_lines(coordindates, frame, stroke, dominant_hand):
+    height, width = frame.shape[:2]
+
+    shoulder = coordindates[f"shoulder_{dominant_hand}"]
+    elbow = coordindates[f"elbow_{dominant_hand}"]
+    wrist = coordindates[f"wrist_{dominant_hand}"]
+
+    get_point = lambda body_part: (
+        int(body_part[0] * width), int(body_part[1] * height)
+    )
+
+    point_shoulder = get_point(shoulder)
+    point_elbow = get_point(elbow)
+    point_wrist = get_point(wrist)
+
+    cv2.line(frame, point_shoulder, point_elbow, STROKE_COLOUR[stroke], 2)
+    cv2.line(frame, point_elbow, point_wrist, STROKE_COLOUR[stroke], 2)
+
+    return frame
