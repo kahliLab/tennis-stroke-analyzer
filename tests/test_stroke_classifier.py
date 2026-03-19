4bff74f13fdc45500aaf95479483867cc0f98c7f
k2 <klaus.kahlfuss@kstp.at>
2026-03-19 20:16:13 +0100

    feat: added test data for stroke classifier tests

---
+++ tests/test_stroke_classifier.py
@@ -0,0 +1,78 @@
+x, y, z = 0.05
+
+pose_data_topspin = [
+    {
+        "shoulder_left": [x, y, z],
+        "shoulder_right": [x, y, z],
+        "elbow_left": [x, 0.07, z],
+        "elbow_right": [x, 0.07, z],
+        "wrist_left": [x, 0.13, z],
+        "wrist_right": [x, 0.13, z],
+    },
+    {
+        "shoulder_left": [x, y, z],
+        "shoulder_right": [x, y, z],
+        "elbow_left": [x, 0.07, z],
+        "elbow_right": [x, 0.07, z],
+        "wrist_left": [x, 0.01, z],
+        "wrist_right": [x, 0.01, z],
+    },
+]
+
+pose_data_slice = [
+    {
+        "shoulder_left": [x, y, z],
+        "shoulder_right": [x, y, z],
+        "elbow_left": [x, 0.07, z],
+        "elbow_right": [x, 0.07, z],
+        "wrist_left": [x, 0.01, z],
+        "wrist_right": [x, 0.01, z],
+    },
+    {
+        "shoulder_left": [x, y, z],
+        "shoulder_right": [x, y, z],
+        "elbow_left": [x, 0.07, z],
+        "elbow_right": [x, 0.07, z],
+        "wrist_left": [x, 0.13, z],
+        "wrist_right": [x, 0.13, z],
+    },
+]
+
+
+pose_data_flat = [
+    {
+        "shoulder_left": [x, y, z],
+        "shoulder_right": [x, y, z],
+        "elbow_left": [x, 0.07, z],
+        "elbow_right": [x, 0.07, z],
+        "wrist_left": [x, 0.10, z],
+        "wrist_right": [x, 0.10, z],
+    },
+    {
+        "shoulder_left": [x, y, z],
+        "shoulder_right": [x, y, z],
+        "elbow_left": [x, 0.07, z],
+        "elbow_right": [x, 0.07, z],
+        "wrist_left": [x, 0.04, z],
+        "wrist_right": [x, 0.04, z],
+    },
+]
+
+pose_data_unknown = [
+    {
+        "shoulder_left": [x, y, z],
+        "shoulder_right": [x, y, z],
+        "elbow_left": [x, 0.17, z],
+        "elbow_right": [x, 0.17, z],
+        "wrist_left": [x, 0.01, z],
+        "wrist_right": [x, 0.01, z],
+    },
+    {
+        "shoulder_left": [x, y, z],
+        "shoulder_right": [x, y, z],
+        "elbow_left": [x, 0.27, z],
+        "elbow_right": [x, 0.27, z],
+        "wrist_left": [x, 0.14, z],
+        "wrist_right": [x, 0.14, z],
+    },
+]
