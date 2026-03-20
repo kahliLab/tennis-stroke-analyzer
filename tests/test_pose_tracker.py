1809befb8703b81d321468b64ab0646e5703c171
k2 <klaus.kahlfuss@kstp.at>
2026-03-20 22:29:12 +0100

    feat: added test for pose_tracker

--- tests/test_pose_tracker.py
+++ tests/test_pose_tracker.py
@@ -1,7 +1,7 @@
 import pytest
 from dataclasses import dataclass
 
-from src.tennis_analyzer.pose_tracker import get_coordinates
+from tennis_analyzer.pose_tracker import get_coordinates
 
 
 @dataclass
@@ -14,3 +14,25 @@ class FakeLandmark:
 class FakeLandmarks:
     def __init__(self):
         self.landmakr = [FakeLandmark(0.5, 0.5, 0.5)] * 33
+
+
+@pytest.fixture
+def fake_landmarks():
+    return FakeLandmark()
+
+def test_get_coordinates():
+    result = get_coordinates(fake_landmarks)
+
+    helper_body_parts_index = {
+        "shoulder_left": 11,
+        "shoulder_right": 12,
+        "elbow_left": 13,
+        "elbow_right": 14,
+        "wrist_left": 15,
+        "wrist_right": 16,
+    }
+    assert len(result) == len(helper_body_parts_index)
+
+    for key, value in result.items():
+        assert key in helper_body_parts_index.keys()
+        assert value == [0.5, 0.5, 0.5]
