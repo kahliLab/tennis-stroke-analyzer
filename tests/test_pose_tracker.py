56980c1fb06e86bbc79ecfff628b239a1cdfbb28
k2 <klaus.kahlfuss@kstp.at>
2026-03-20 22:04:12 +0100

    feat: created classed to mock landmark.landmarks

--- tests/test_pose_tracker.py
+++ tests/test_pose_tracker.py
@@ -1,5 +1,16 @@
 import pytest
+from dataclasses import dataclass
 
 from src.tennis_analyzer.pose_tracker import get_coordinates
 
 
+@dataclass
+class FakeLandmark:
+    x: float
+    y: float
+    z: float
+
+
+class FakeLandmarks:
+    def __init__(self):
+        self.landmakr = [FakeLandmark(0.5, 0.5, 0.5)] * 33
