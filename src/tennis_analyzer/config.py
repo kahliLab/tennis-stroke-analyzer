688b4402a690a8ff8d51d4190b0cd195d05a5e6b
k2 <klaus.kahlfuss@kstp.at>
2026-03-22 12:19:54 +0100

    refactor: add all_landmarks info

--- src/tennis_analyzer/config.py
+++ src/tennis_analyzer/config.py
@@ -2,14 +2,44 @@ THRESHOLD = 0.05
 
 STROKE_COLOUR = {"Topspin": (0, 0, 255), "Flat": (0, 255, 0), "Slice": (255, 0, 0)}
 
-BODY_PARTS_INDEXES = {
+
+ALL_LANDMARKS = {
+    "nose": 0,
+    "left_eye_inner": 1,
+    "left_eye": 2,
+    "left_eye_outer": 3,
+    "right_eye_inner": 4,
+    "right_eye": 5,
+    "right_eye_outer": 6,
+    "left_ear": 7,
+    "right_ear": 8,
+    "mouth_left": 9,
+    "mouth_right": 10,
     "shoulder_left": 11,
     "shoulder_right": 12,
     "elbow_left": 13,
     "elbow_right": 14,
     "wrist_left": 15,
     "wrist_right": 16,
+    "pinky_left": 17,
+    "pinky_right": 18,
+    "index_left": 19,
+    "index_right": 20,
+    "thumb_left": 21,
+    "thumb_right": 22,
+    "hip_left": 23,
+    "hip_right": 24,
+    "knee_left": 25,
+    "knee_right": 26,
+    "ankle_left": 27,
+    "ankle_right": 28,
+    "heel_left": 29,
+    "heel_right": 30,
+    "foot_index_left": 31,
+    "foot_index_right": 32,
 }
+
+PARTS = ["shoulder", "elbow", "wrist"]
 
 INPUT_PATH = "./data/input.mp4"
 OUTPUT_PATH = "./output/output.mp4"
