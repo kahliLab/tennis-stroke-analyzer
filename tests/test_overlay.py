9c730680c6d82e3a871657a7deefc94666b35fac
k2 <klaus.kahlfuss@kstp.at>
2026-04-19 18:49:26 +0200

    chore: isort and black

--- tests/test_overlay.py
+++ tests/test_overlay.py
@@ -1,21 +1,20 @@
 import numpy as np
 import pytest
 
-from tennis_analyzer.overlay import put_text, draw_lines
 from tennis_analyzer.config import ALL_LANDMARKS
+from tennis_analyzer.overlay import annotate_frame, draw_lines, put_text
 
 
 @pytest.fixture
 def fake_frame():
     return np.zeros((480, 640, 3), dtype=np.uint8)
 
+
 @pytest.fixture
 def fake_coordinates():
-    fake_coordinates = {
-        key: [0.5, 0.3, 0.0] for key in ALL_LANDMARKS.keys()
-    }
+    fake_coordinates = {key: [0.5, 0.3, 0.0] for key in ALL_LANDMARKS.keys()}
     return fake_coordinates
-    
+
 
 def test_put_text(fake_frame, fake_coordinates):
     spin = "Flat"
@@ -33,6 +32,17 @@ def test_draw_lines(fake_frame, fake_coordinates):
 
     original_frame = fake_frame.copy()
     result_frame = draw_lines(fake_coordinates, fake_frame, spin, dominant_hand)
+
+    assert result_frame is not None
+    assert not np.array_equal(original_frame, result_frame)
+
+
+def test_annotate_frame(fake_frame, fake_coordinates):
+    spin = "Flat"
+    dominant_hand = "left"
+
+    original_frame = fake_frame.copy()
+    result_frame = annotate_frame(spin, fake_frame, fake_coordinates, dominant_hand)
 
     assert result_frame is not None
     assert not np.array_equal(original_frame, result_frame)
