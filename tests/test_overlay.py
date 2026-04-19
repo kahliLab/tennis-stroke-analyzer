e47f8917a82905d7ec91573ea8051844e9bdf0dc
k2 <klaus.kahlfuss@kstp.at>
2026-04-19 18:02:46 +0200

    test: copy original frame for making comparison work

--- tests/test_overlay.py
+++ tests/test_overlay.py
@@ -13,7 +13,8 @@ def test_put_text(fake_frame):
     fake_coordinates = {"nose": [0.5, 0.3, 0.0]}
     spin = "Flat"
     
+    original_frame = fake_frame.copy()
     result_frame = put_text(fake_coordinates, spin, fake_frame)
 
     assert result_frame is not None
-    assert not np.array_equal(fake_frame, result_frame)
+    assert not np.array_equal(original_frame, result_frame)
