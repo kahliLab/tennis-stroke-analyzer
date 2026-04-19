645a68f64e620f8ced8a846066c541d6918d0eee
k2 <klaus.kahlfuss@kstp.at>
2026-04-19 18:01:01 +0200

    test: add test for overlay - put text

---
+++ tests/test_overlay.py
@@ -0,0 +1,19 @@
+import numpy as np
+import pytest
+
+from tennis_analyzer.overlay import put_text
+
+
+@pytest.fixture
+def fake_frame():
+    return np.zeros((480, 640, 3), dtype=np.uint8)
+
+
+def test_put_text(fake_frame):
+    fake_coordinates = {"nose": [0.5, 0.3, 0.0]}
+    spin = "Flat"
+    
+    result_frame = put_text(fake_coordinates, spin, fake_frame)
+
+    assert result_frame is not None
+    assert not np.array_equal(fake_frame, result_frame)
