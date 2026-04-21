62055aa33079db306a431afc0c876ec5d908948b
k2 <klaus.kahlfuss@kstp.at>
2026-04-21 21:29:00 +0200

    test: add test for exporting video and gif

--- tests/test_video_processor.py
+++ tests/test_video_processor.py
@@ -1,7 +1,14 @@
+import os
+
 import numpy as np
 import pytest
 
-from tennis_analyzer.video_processor import load_video, extract_frames
+from tennis_analyzer.video_processor import (
+    export_gif,
+    export_video,
+    extract_frames,
+    load_video,
+)
 
 
 def test_load_video_invalid_path():
@@ -39,3 +46,17 @@ def fake_cap(fake_frame):
 def test_extract_frames(fake_cap):
     frames = extract_frames(fake_cap)
     assert isinstance(frames, list)
+
+
+def test_export_video(fake_frame, tmp_path):
+    output_path = tmp_path / "test.mp4"
+
+    export_video(30, [fake_frame], output_path)
+    assert os.path.exists(output_path)
+
+
+def test_export_gif(fake_frame, tmp_path):
+    output_path = tmp_path / "test.gif"
+
+    export_gif([fake_frame], output_path)
+    assert os.path.exists(output_path)
